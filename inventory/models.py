10f872c84989a2f89b07c256fc2c4b7f51bb90a6
Adam Dasovich <adamdasovich@gmail.com>
2024-08-13 10:29:17 -0400

    completed main models

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=30)
+    quantity = models.FloatField()
+    unit = models.CharField(max_length=15)
+    unit_price = models.FloatField()
+
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=30)
+    price = models.FloatField()
+
+
+class RecipeRequirement(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    Ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.FloatField()
+
+
+
+class Purchase(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    time_stamp = models.DateField()
