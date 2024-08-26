cfa12322b69013c00eef736ae2572affff0086db
Adam Dasovich <adamdasovich@gmail.com>
2024-08-26 10:09:32 -0400

    fully operational crud for ingredients

--- inventory/urls.py
+++ inventory/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('ingredient/list/', views.IngredientList.as_view(), name='ingredientlist'),
     path('ingredient/new/', views.IngredientCreate.as_view(), name='ingredientcreate'),
-    path('ingredient/<pk>/update/', views.IngredientUpdate.as_view(), name='ingredientupdate'),
-    path('ingredient/<pk>/delete/', views.IngredientDelete.as_view(), name='ingredientdelete'),
+    path('ingredient/update/<pk>', views.IngredientUpdate.as_view(), name='ingredientupdate'),
+    path('ingredient/delete/<pk>', views.IngredientDelete.as_view(), name='ingredientdelete'),
 ]
 
--- inventory/views.py
+++ inventory/views.py
@@ -3,6 +3,7 @@ from .models import Ingredient, MenuItem, RecipeRequirement, Purchase
 from django.views.generic import ListView, TemplateView
 from django.views.generic import CreateView, UpdateView, DeleteView
 from .forms import IngredientForm
+from django.urls import reverse
 
 # Create your views here.
 class HomeView(TemplateView):
@@ -19,10 +20,13 @@ class IngredientCreate(CreateView):
 
 class IngredientUpdate(UpdateView):
     model = Ingredient
-    template_name = 'inventory/ingredient_update'
+    template_name = 'inventory/ingredient_update.html'
     form_class = IngredientForm
 
 class IngredientDelete(DeleteView):
     model = Ingredient
-    template_name = 'inventory/ingridient_delete'
-    form_class = IngredientForm
+    template_name = 'inventory/ingredient_delete.html'
+
+    def get_success_url(self):
+        return reverse('ingredientlist')
+    
